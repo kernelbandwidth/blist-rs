34f69a118596fb2d03645fff9395f240b61f273d
James Crooks <crooks1379@gmail.com>
2017-02-14 12:11:51 -0600

    Adding docs folder for gh-pages

---
+++ docs/implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tlist"] = ["impl&lt;'a, T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='tlist/struct.Iter.html' title='tlist::Iter'>Iter</a>&lt;'a, T&gt; <span class='where'>where T: 'a</span>","impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='tlist/struct.IntoIter.html' title='tlist::IntoIter'>IntoIter</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ docs/implementors/core/iter/traits/trait.ExactSizeIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tlist"] = ["impl&lt;'a, T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.ExactSizeIterator.html' title='core::iter::traits::ExactSizeIterator'>ExactSizeIterator</a> for <a class='struct' href='tlist/struct.Iter.html' title='tlist::Iter'>Iter</a>&lt;'a, T&gt; <span class='where'>where T: 'a</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
